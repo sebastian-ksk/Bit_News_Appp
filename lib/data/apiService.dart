91b4c67f93ecd358b7ece42971ea984c7ff67d67
sebastian_castellanos <sebastiancastell371@gmail.com>
2021-02-23 15:35:14 -0500

    api rest funcionando newsapi.org

--- lib/data/apiService.dart
+++ lib/data/apiService.dart
@@ -1,27 +1,25 @@
 import 'package:app1/models/news.dart';
 import 'package:dio/dio.dart';
-
 import 'package:flutter/material.dart';
 
-class ApiService{
-
-  String UrlForApi(optionUrl){
-    switch (optionUrl){
+class ApiService {
+  String UrlForApi(optionUrl) {
+    switch (optionUrl) {
       //All articles about Tesla from the last month, sorted by recent first
       case 1:
-        return 'https://newsapi.org/v2/everything?q=tesla&from=2021-01-11&sortBy=publishedAt&apiKey=1e31d79a1b5f4cd88a65e61474ef93b7';
+        return 'http://newsapi.org/v2/everything?q=tesla&from=2021-01-23&sortBy=publishedAt&apiKey=d03cc61aff3941b6892cc06a05c29c34';
       //Top business headlines in the US right now
       case 2:
-        return 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1e31d79a1b5f4cd88a65e61474ef93b7';
+        return 'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d03cc61aff3941b6892cc06a05c29c34';
       //All articles mentioning Apple from yesterday, sorted by popular publishers first
       case 3:
-        return 'https://newsapi.org/v2/everything?q=apple&from=2021-02-10&to=2021-02-10&sortBy=popularity&apiKey=1e31d79a1b5f4cd88a65e61474ef93b7';
+        return 'http://newsapi.org/v2/everything?q=apple&from=2021-02-22&to=2021-02-22&sortBy=popularity&apiKey=d03cc61aff3941b6892cc06a05c29c34';
       //Top headlines from TechCrunch right now
       case 4:
-        return 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=1e31d79a1b5f4cd88a65e61474ef93b7';
+        return 'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=d03cc61aff3941b6892cc06a05c29c34';
       //All articles published by the Wall Street Journal in the last 6 months, sorted by recent first
       case 5:
-        return 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=1e31d79a1b5f4cd88a65e61474ef93b7';
+        return 'http://newsapi.org/v2/everything?domains=wsj.com&apiKey=d03cc61aff3941b6892cc06a05c29c34';
     }
   }
 
@@ -30,9 +28,9 @@ class ApiService{
 
     Response response = await dio.get(UrlForApi(optionUrl));
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       return NewsApiModel.fromjson(response.data);
-    }else{
+    } else {
       print('Algo ha salido mal');
     }
   }
